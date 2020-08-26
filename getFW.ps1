d1ab418b59869756cd45823e0ce163bb2af9cfd4
True-ps <67391353+True-ps@users.noreply.github.com>
2020-08-26 12:59:49 +0200

    Add files via upload

---
+++ getFW.ps1
@@ -0,0 +1,34 @@
+﻿$getRulesStatus = Get-NetFirewallProfile | Select-Object Name , Enabled
+
+#checks if a Windows Event log source with the give name already exists.
+$logFileExists = [System.Diagnostics.EventLog]::SourceExists("WindowsFirewall");
+#if the log source exists, it uses it. if it doesn't, it creates it.
+if (! $logFileExists)
+{New-EventLog -LogName Application -Source "WindowsFirewall"}
+
+
+
+foreach ($rule in $getRulesStatus)
+
+{
+
+if ($rule.Enabled -eq $true)
+{
+
+Write-Host $rule
+
+<#$infoMessage = $rule.name
+
+ Write-Output "`n`n$infoMessage"
+
+    Write-EventLog -LogName Application -Source "WindowsFirewall" -EntryType Information -EventID 2368 -Message ($infoMessage)
+    #>
+}
+elseif ($rule.Enabled -eq $false)
+
+{ 
+$errorMessage = "$rule.Name is disabled" 
+Write-Host "`n`n$errorMessage"
+    Write-EventLog -LogName Application -Source "WindowsFirewall" -EntryType Error -EventID 2365 -Message ($errorMessage)
+}
+}
